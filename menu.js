4e3bb75911f40e0834efb7774d03349d571168aa
Jens Andersen <jashjemmeside@gmail.com>
2021-11-24 13:32:09 +0100

    change menu function

--- menu.js
+++ menu.js
@@ -11,11 +11,10 @@ burger.addEventListener("click", function () {
 function mobilMenu() {
 	if (menu.style.display !== "flex") {
 		menu.style.display = "flex";
-		document.querySelectorAll('.menu a').forEach(link => {
-			link.addEventListener('click', function () {
-				location.href;
-			})
-		});
+		let link = document.querySelectorAll(".menu a");
+		link.addEventListener("click", function () {
+			location.href;
+		})
 	} else {
 		menu.style.display = "none";
 	}
